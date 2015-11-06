7b0d8d09802e40de855433d8e127448878f73c59
Kunal Verma <kunal-verma@live.in>
2015-11-06 01:23:20 -0500

    Transloc10

---
+++ GetAllAgency.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.ApplicationModel;
+using Windows.Storage;
+using Windows.Storage.Streams;
+using Newtonsoft.Json;
+
+namespace Transloc10Test
+{
+    class GetAllAgency
+    {
+        
+        public static async Task<RootObject> GetAll()
+        {
+            var file = checkFile();
+            if (file == false)
+            {
+                string URL = "https://transloc-api-1-2.p.mashape.com/agencies.json";
+                string urlParams = "";
+                HttpClient client = new HttpClient();
+
+                // Add headers for JSON format.
+                client.BaseAddress = new Uri(URL, UriKind.Absolute);
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                client.DefaultRequestHeaders.Add("X-Mashape-Key", "y1ussTz5ltmshVQrmpOiaCRBkpxip1zmwU3jsnX2QyR0SA2QaH");
+                var respose = await client.GetAsync(urlParams);
+                string result = await respose.Content.ReadAsStringAsync();
+                storeInMem(result);
+            }
+            string result1 = ReadTextFile();
+            var serializer = new DataContractJsonSerializer(typeof(RootObject));
+            var ms = new MemoryStream(Encoding.UTF8.GetBytes(result1));
+            var data = (RootObject)serializer.ReadObject(ms);
+            return data;
+            
+        }
+
+        
+
+        static bool checkFile()
+        {
+            var localSettings = ApplicationData.Current.LocalSettings;
+            if (localSettings.Values.ContainsKey("agencies"))
+                return true;
+            else
+                return false;
+        }
+
+        static void storeInMem(string result)
+        {
+            var localSettings = ApplicationData.Current.LocalSettings;
+
+            // Create a simple setting
+
+            localSettings.Values["agencies"] = result;
+
+            // Read data from a simple setting
+
+        }
+
+        //static async Task<string> readFromMem()
+
+
+        
+        public static string ReadTextFile()
+        {
+            var localSettings = ApplicationData.Current.LocalSettings;
+            string value = (String)localSettings.Values["agencies"];
+            return value;
+        }
+
+
+        public static async Task<RootObject> GetClose(double lat, double lon, int rad = 75)
+        {
+            string URL = "https://transloc-api-1-2.p.mashape.com/agencies.json";
+            string urlParams = (String)String.Format("?geo_area={0}%2C+{1}%7C{2}", lat.ToString(), lon.ToString(), rad.ToString());
+            HttpClient client = new HttpClient();
+
+            // Add headers for JSON format.
+            client.BaseAddress = new Uri(URL, UriKind.Absolute);
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            client.DefaultRequestHeaders.Add("X-Mashape-Key", "y1ussTz5ltmshVQrmpOiaCRBkpxip1zmwU3jsnX2QyR0SA2QaH");
+            var respose = await client.GetAsync(urlParams);
+            var result = await respose.Content.ReadAsStringAsync();
+            var serializer = new DataContractJsonSerializer(typeof(RootObject));
+
+            var ms = new MemoryStream(Encoding.UTF8.GetBytes(result));
+            var data = (RootObject)serializer.ReadObject(ms);
+            return data;
+        }
+    }
+
+    [DataContract]
+    public class Position
+    {
+        [DataMember]
+        public double lat { get; set; }
+        [DataMember]
+        public double lng { get; set; }
+    }
+
+    [DataContract]
+    public class Datum
+    {
+        [DataMember]
+        public string name { get; set; }
+        [DataMember]        
+        public string short_name { get; set; }
+        [DataMember]
+        public string phone { get; set; }
+        [DataMember]
+        public string language { get; set; }
+        [DataMember]
+        public string agency_id { get; set; }
+        [DataMember]
+        public string long_name { get; set; }
+        [DataMember]
+        public string url { get; set; }
+        [DataMember]
+        public string timezone { get; set; }
+        [DataMember]
+        public Position position { get; set; }
+    }
+
+    [DataContract]
+    public class RootObject
+    {
+        [DataMember]
+        public int rate_limit { get; set; }
+        [DataMember]
+        public int expires_in { get; set; }
+        [DataMember]
+        public string api_latest_version { get; set; }
+        [DataMember]
+        public string generated_on { get; set; }
+        [DataMember]
+        public List<Datum> data { get; set; }
+        [DataMember]
+        public string api_version { get; set; }
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using System.Net.Http;
+using System.Net;
+using System.Net.Http.Headers;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Transloc10Test
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            abc();
+        }
+
+        private async void  abc()
+        {
+            RootObject ro = await GetAllAgency.GetAll();
+            //RootObject roC = await GetAllAgency.GetClose(35.771560, -78.692563);
+            var realList = ro.data;
+            lb.ItemsSource = realList;
+
+            //var products = from id1 in ro.data
+            //               join id2 in roC.data on id1.agency_id equals id2.agency_id
+            //               select id1;
+            
+            //foreach(var p in products)
+            //{
+            //    foreach(var item in realList)
+            //    {
+            //        if (p == item)
+            //            lb.SelectedItems.Add(item);
+            //    }
+            //}
+
+            //var selList = lb.SelectedItems;
+            //sel.ItemsSource = selList;
+            //GreekGod greekGod = (GreekGod)(listBox.Items[0]);
+
+        }
+
+        
+    }
+}
+    
